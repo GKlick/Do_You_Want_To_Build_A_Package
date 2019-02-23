b905650c4916073c6a98495bc8cc6d52635f1eac
GKlick <gabrielle.klick@gmail.com>
2019-02-23 15:49:33 -0600

    final update for the day

--- pack1/R/zoosounds.R
+++ pack1/R/zoosounds.R
@@ -1,8 +1,21 @@
+#' Title
+#'
+#' @param animal
+#' @param sound
+#'
+#' @return the sound of the animal
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' goToTheZoo('cat',"meow")
+#The*cat*goes*meow*meow*!
+#' }
 goToTheZoo <- function(animal, sound){
 
   assertthat::assert_that(
     assertthat::is.string(animal),
-    assertthat::is.string(sound))
+    assertthat::is.string(usesound))
 
   glue::glue("The",animal,"goes",sound,sound,"!", .sep = "**")
 }
