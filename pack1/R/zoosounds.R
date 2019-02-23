78197a39e6c7a48e9b50a6a66d41c492f5ba462f
GKlick <gabrielle.klick@gmail.com>
2019-02-23 12:00:27 -0600

    first pack added

---
+++ pack1/R/zoosounds.R
@@ -0,0 +1,8 @@
+goToTheZoo <- function(animal, sound){
+
+  assertthat::assert_that(
+    assertthat::is.string(animal),
+    assertthat::is.string(sound))
+
+  glue::glue("The",animal,"goes",sound,sound,"!", .sep = " ")
+}
